d8b1cdca4245b2c7e3398e41fce471df9bb96f9d
mt1643 <39344458+mt1643@users.noreply.github.com>
2022-05-22 23:51:08 +0900

    feature disp qr code

--- app/Http/Controllers/QrCodeController.php
+++ app/Http/Controllers/QrCodeController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use chillerlan\QRCode\QRCode;
 
 class QrCodeController extends Controller
 {
     public function index()
     {
-        return view('qr');
+        $url = 'https://www.google.com';
+        $data = (new QRCode())->render($url);
+        return view('qr', ['url' => $data]);
     }
 }
--- resources/views/qr.blade.php
+++ resources/views/qr.blade.php
@@ -1,1 +1,2 @@
-QR text
+QR img
+<img src={{$url}}>
